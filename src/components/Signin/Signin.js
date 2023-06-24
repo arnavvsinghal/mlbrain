88abc418e4220865c7cda950533dff515f4662ad
Arnav Singhal <94188860+arnavvsinghal@users.noreply.github.com>
2023-06-24 17:48:19 +0530

    changed the background

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import Form from '../Form/Form'
 import './sigin.css'
+import Group from '../Signin/images/Group.png';
 export default class Signin extends Component {
     render() {
         console.log(window.innerWidth);
         return (
             <div className='signIn'>
                 <div className='image'>
+                    <h1>HELLO!</h1>
                     <p>This is a face recognition website.
                         It can be used to indentify human faces in images.</p>
-                    {/* <img src={backgroundimg} alt='background' /> */}
+                    <img src={Group} alt='background' />
                 </div>
                 <div className='signInForm'>
                     <Form />
