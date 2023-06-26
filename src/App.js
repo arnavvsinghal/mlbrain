56b4025111762333e283cafb8a5f86a71ba5d3ba
Arnav Singhal <94188860+arnavvsinghal@users.noreply.github.com>
2023-06-26 20:46:51 +0530

    api page setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Signin from './components/Signin/Signin';
 import SignoutButton from './components/SignoutButton/SignoutButton';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import backgroundimg from './Group.png';
+import './App.css';
+
 const setupAPIRequest = (imageURL) => {
 
   const PAT = '55c31f0e9520406aa82a8e3d8c293c28';
@@ -41,6 +44,7 @@ export default class App extends Component {
       input: '',
       imageURL: '',
       box: {},
+      instructions:'Hey there! Paste an image link in the search box above and click "Detect".',
     }
   }
   calculateFaceLocation = (data) => {
@@ -53,10 +57,10 @@ export default class App extends Component {
     const imageHeight = Number(image.height);
     console.log(imageWidth, imageHeight);
     const box = {
-      leftCol: (face.left_col) * imageWidth+Number(rect.left),
-      rightCol: Number(window.innerWidth)-Number(rect.right)+((1-face.right_col)* imageWidth),
-      topRow: (face.top_row) * imageHeight+Number(rect.top),
-      bottomRow:Number(window.innerHeight) -Number(rect.bottom)+((1-face.bottom_row) * imageHeight),
+      leftCol: (face.left_col) * imageWidth + Number(rect.left),
+      rightCol: Number(window.innerWidth) - Number(rect.right) + ((1 - face.right_col) * imageWidth),
+      topRow: (face.top_row) * imageHeight + Number(rect.top),
+      bottomRow: Number(window.innerHeight) - Number(rect.bottom) + ((1 - face.bottom_row) * imageHeight),
     };
     return box;
   }
@@ -71,20 +75,32 @@ export default class App extends Component {
   }
   onButtonSubmit = () => {
     this.setState({ imageURL: this.state.input });
+    this.setState({ instructions: '' });
+    this.setState({ box: '' });
     console.log('click submit');
     fetch(`https://api.clarifai.com/v2/models/face-detection/outputs`, setupAPIRequest(this.state.input))
       .then(response => response.json())
       .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
-      .catch(error => console.log('error', error));
+      .catch((error) => {
+        console.log(error);
+        this.setState({instructions:'Invalid URL! Please try again.'});
+      });
   }
   render() {
     return (
-      <div>
+      <div className='App'>
         {/* <Signin /> */}
         <SignoutButton />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageURL={this.state.imageURL} box={this.state.box} />
-        {/* console.log(this.state.input); */}
+        <div className='apiWrapper'>
+          <div className='submitButton'>
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+            <FaceRecognition imageURL={this.state.imageURL} box={this.state.box} instructions={this.state.instructions}/>
+          </div>
+          <div className='img-container'>
+            <p className='instructions'>Make sure the image links end with jpg or png. Smiley!</p>
+            <img alt='' src={backgroundimg} />
+          </div>
+        </div>
       </div>
     )
   }
